4a9651fb4abafccdefa52565ab80f4e9a947bb29
Kaveri Chougule <chougulekaveri123@gmail.com>
2024-01-09 00:35:14 +0530

    Initial Commit

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,21 +2,21 @@ import React from "react";
 import "./Body.css";
 function Body(props) {
     function HandleClick(){
-
+        props.setCount((prev)=>prev+1);
     }
   return (
     <div className="container">
       <div className="wrapper">
         <div className="textInput">
-          <input type="text" />
+          <input type="text" value={props.inputText} className="InputText" onChange={((e)=>{
+            props.setInputText(e.target.value);
+          })} />
           <button onClick={HandleClick}>Generate</button>
         </div>
-        
+        </div>  
         <div>
-          <img src="" alt="Generated Img" />
+          <img src={props.imageurl} alt="Generated Img" />
         </div>
-      </div>
-      
     </div>
   );
 }
