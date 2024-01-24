b43c02525012fed07f1bff200134abcc7682e38a
Kaveri Chougule <chougulekaveri123@gmail.com>
2024-01-24 18:10:46 +0530

    api updates

--- src/App.js
+++ src/App.js
@@ -1,43 +1,59 @@
-import { useState,useEffect } from 'react';
-import axios from 'axios'
-import './App.css';
-import Body from "./Components/Body";
-function App() {
-  document.title="Image Generator"
-  const [inputText, setInputText] = useState('');
-  const [imageURL, setImageURL] = useState('');
-  let [count,setCount]=useState(0);
-  const API_TOKEN = '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'; // Replace with your actual API token
+// import { useState,useEffect } from 'react';
+// import axios from 'axios'
+// import './App.css';
+// import Body from "./Components/Body";
+// function App() {
+//   document.title="Image Generator"
+//   const [inputText, setInputText] = useState('');
+//   const [imageURL, setImageURL] = useState('');
+//   let [count,setCount]=useState(0);
+//   const API_TOKEN = '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'; // Replace with your actual API token
 
-  useEffect(() => {
-   function fetchData(){
-      axios
-        .post(
-          'https://api-inference.huggingface.co/models/prompthero/openjourney-v4',
-          {
-            inputs: inputText,
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${API_TOKEN}`,
-            },
-            responseType: 'blob', // Set responseType to 'blob' to get binary data
-          }
-        )
-        .then((response) => {
-          const imageUrl = URL.createObjectURL(response.data);
-          setImageURL(imageUrl);
-        })
-        .catch((error) => {
-          console.error('Error generating image:', error);
-        });
-    }
-    fetchData()
-  }, [count]);
+//   useEffect(() => {
+//    function fetchData(){
+//       axios
+//         .post(
+//           'https://api-inference.huggingface.co/models/prompthero/openjourney-v4',
+//           {
+//             inputs: inputText,
+//           },
+//           {
+//             headers: {
+//               Authorization: `Bearer ${API_TOKEN}`,
+//             },
+//             responseType: 'blob', // Set responseType to 'blob' to get binary data
+//           }
+//         )
+//         .then((response) => {
+//           const imageUrl = URL.createObjectURL(response.data);
+//           setImageURL(imageUrl);
+//         })
+//         .catch((error) => {
+//           console.error('Error generating image:', error);
+//         });
+//     }
+//     fetchData()
+//   }, [count]);
+  
+//   return (
+//     <div className="App">
+//       <Body setCount={setCount} imageurl={imageURL} inputText={inputText} setInputText={setInputText} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import axios from 'axios';
+import React from 'react';
+import Body from './Components/Body';
+
+function App(props) {
+  let bodyObj={inputs: "Astronaut riding a horse"}
   
   return (
-    <div className="App">
-      <Body setCount={setCount} imageurl={imageURL} inputText={inputText} setInputText={setInputText} />
+    <div>
+      <Body />
     </div>
   );
 }
--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,22 +1,93 @@
-import React from "react";
+// import React from "react";
 import "./Body.css";
+// function Body(props) {
+//     function HandleClick(){
+//         props.setCount((prev)=>prev+1);
+//     }
+//   return (
+//     <div className="container">
+//       <div className="wrapper">
+//         <div className="textInput">
+//           <input type="text" value={props.inputText} className="InputText" onChange={((e)=>{
+//             props.setInputText(e.target.value);
+//           })} />
+//           <button onClick={HandleClick}>Generate</button>
+//         </div>
+//         </div>  
+//         <div>
+//           <img src={props.imageurl} alt="Generated Img" />
+//         </div>
+//     </div>
+//   );
+// }
+
+// export default Body;
+
+import React, { useEffect,useState } from 'react';
+import axios from 'axios';
 function Body(props) {
-    function HandleClick(){
-        props.setCount((prev)=>prev+1);
+  
+  const [inputText, setInputText] = useState("");
+  const [imageURL, setImageURL] = useState("");
+  const [submitted, setSubmitted] = useState(0);
+  const API_TOKEN = "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"; 
+
+  useEffect(() => {
+    const fetchData = async ()=> {
+      axios
+        .post(
+          "https://api-inference.huggingface.co/models/prompthero/openjourney-v4",
+          {
+            inputs: inputText,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${API_TOKEN}`,
+            },
+            responseType: "blob", // Set responseType to 'blob' to get binary data
+          }
+        )
+        .then((response) => {
+          const imageUrl = URL.createObjectURL(response.data);
+          setImageURL(imageUrl);
+        })
+        .catch((error) => {
+          console.error("Error generating image:", error);
+        });
     }
+    fetchData();
+  }, [submitted]);
+
+  const callApi = (e)=>{
+    e.preventDefault();
+    setImageURL("");
+    if (inputText === "") return;
+    setSubmitted(prev => prev + 1);
+  }
   return (
-    <div className="container">
-      <div className="wrapper">
-        <div className="textInput">
-          <input type="text" value={props.inputText} className="InputText" onChange={((e)=>{
-            props.setInputText(e.target.value);
-          })} />
-          <button onClick={HandleClick}>Generate</button>
+    <div className="App">
+      <h1 className="heading">Image Generation App</h1>
+      <div className="formContainer">
+        <form onSubmit={callApi}>
+          <input
+            type="text"
+            placeholder="Enter text to generate image..."
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            className="inputBox"
+          />
+          <button className="submit">
+            Generate Image <i class="fa-solid fa-circle-down"></i>
+          </button>
+        </form>
+      </div>
+      {imageURL !== "" ? (
+        <div className="imageContainer">
+          <img src={imageURL} alt="Generated" className="w-full" />
         </div>
-        </div>  
-        <div>
-          <img src={props.imageurl} alt="Generated Img" />
-        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
